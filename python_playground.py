4e02eeb9396fdc2c7bee546e67f5a124f6569f6b
Nadia Bharose <nadiadevi93@gmail.com>
2020-06-24 23:19:49 -0400

    Learning Python Edureka hour 2

---
+++ python_playground.py
@@ -0,0 +1,33 @@
+# this is a comment
+a = 20
+b = 25
+print(a+b)
+
+x = 10.45
+print type(x)
+
+name = "HELLO"
+print name[0]
+
+mylist = [12, 34, 56, 78, 'eat pie', 'ilikeamiri']
+print mylist
+
+mylist.append('purple')
+print mylist
+
+mylist.insert(3, 'se')
+print mylist
+
+poop = {1: 'poop1', 2: 'joop', 3: 'goop'}
+print poop.get(1)
+print loop[2]
+
+poop[1] = 'fiber'
+print poop
+
+loop['fourth'] = 'hoppp'
+print poop
+
+
+range(0, 10)
+print list(range(11))
